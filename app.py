ad3a9560cfc179f8c15fbd83c98bf7d40177882a
eledualbano <nedualbano@gmail.com>
2026-06-24 13:10:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ CSV_URL_ACTIVOS = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/expo
 CSV_URL_HISTORIAL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=csv&gid={GID_HISTORIAL}"
 
 # Rangos de datos
-FILA_INICIAL = 196 
-FILA_FINAL = 217
+FILA_INICIAL = 193 
+FILA_FINAL = 215
 RANGO_ON = range(236, 243)      # Filas 239 a 245 (ajustar si es necesario)
 RANGO_CEDEAR = range(245, 259)  # Filas 248 a 261 (ajustar si es necesario)
 
